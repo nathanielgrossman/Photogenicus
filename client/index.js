33966dcf8155ce356e423def05edb6fa51822f1e
briantaylor <brian.taylor818@gmail.com>
2018-07-07 11:43:46 -0700

    :construction: push of current work for frontend

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { browserHistory, Router } from 'react-router-dom';
+import routes from './routes';
 
 
 ReactDOM.render(
-    <App />
-  , document.querySelector('.container'));
+  <App />
+    
+  , document.querySelector('.App'));
---
+++ client/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+
+export default (
+  <Route path="/" component={App} />
+)
+
+
+
+// <BrowserRouter>
+    
+// <Router path="/" component={NewRoute} />
+// <Router path="/images" component={NewRoute} />
+// </BrowserRouter>

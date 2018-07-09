49b6d8f0fab7498e3e478276ba1ed8dadf6a6f6f
Cam <>
2018-07-09 15:50:35 -0700

    uploading box

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
 const app = express();
 const routes = require('./routes/ImageRoutes');
 
+// needed to allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// this is to be able to serve static images from express
 app.use(express.static(__dirname + "/public/true"));
 app.use(express.static(__dirname + "/public/false"));
 app.use(express.static(__dirname + "/public/fresh"));
 app.use(express.static(__dirname + "/public"));
+
+// for the file upload
 app.use(fileUpload());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/', routes);
 
 const PORT = process.env.PORT || 5000;

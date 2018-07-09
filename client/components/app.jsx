c37779da286f8e83aa43490020cb38591390e3b3
nathanielgrossman <nategrossman@mac.com>
2018-07-09 11:31:29 -0700

    added loss counter

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,16 +9,24 @@ export default class App extends Component {
 
 
   train() {
-    // let loss = document.getElementById('loss')
-    this.props.classifier.train() 
-
+    let totalLoss = 0;
+    let loss = document.getElementById('loss');
+    this.props.classifier.train(function(lossValue) {
+    if (lossValue) {
+      totalLoss = lossValue;
+      loss.innerHTML = 'Loss: ' + totalLoss;
+    } else {
+      loss.innerHTML = 'Done Training! Final Loss: ' + totalLoss;
+    }
+  }); 
+    
   }
 
   render() {
 
     return (
       <div>
-      <button onClick={this.train.bind(this)}>Train</button>
+      <button onClick={this.train.bind(this)}>Train</button><span id="loss"></span>
       <TrueImages classifier={this.props.classifier}/>
       <FalseImages classifier={this.props.classifier}/>
       <FreshImages classifier={this.props.classifier}/>

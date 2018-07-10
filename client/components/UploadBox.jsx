39e7b1df91e44f7546c7fdf20eca61dba604a928
nathanielgrossman <nategrossman@mac.com>
2018-07-09 18:19:57 -0700

    made upload box collapse

--- client/components/UploadBox.jsx
+++ client/components/UploadBox.jsx
@@ -3,16 +3,34 @@ import React, { Component } from 'react';
 export default class UploadBox extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      heightClass: '',
+    }
+  }
+
+  collapseDiv() {
+    this.setState((current) => {
+      if (current.heightClass === '') {
+              current.heightClass = 'collapsed';
+      } else {
+        current.heightClass = '';
+      }
+      return current;
+    })
   }
 
   render() {
     return (
-      <div id="uploadbox">
+      <div className={this.state.heightClass}>
+      <button className="btn-dark" onClick={this.collapseDiv.bind(this)}>Collapse</button>
+      <div id="uploadbox" >
+
             <form action="http://localhost:5000/upload" className="dropzone" method="post" encType="multipart/form-data">
                 <div className="fallback">
                     <input name="file" type="file" multiple />
                 </div>
             </form>    
+     </div>
      </div>
     )
   }

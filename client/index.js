a1f87612933f4685fd9bb5cebc33fa2711b84386
nathanielgrossman <nategrossman@mac.com>
2018-07-09 18:00:04 -0700

    added styling and column layout

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
+
 // import { browserHistory, Router } from 'react-router-dom';
 // import routes from './routes';
 
